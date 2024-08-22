d748ca7ec7534df85f027d34462e3332ede14202
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-22 09:17:29 -0500

    select targeted classes and store them in a variable

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-'use strict';
+"use strict";
+
+// First, select all the class elements we need (.modal, .overlay, .close-modal, .show-modal)and store them into variables
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsShowModal = document.querySelectorAll(".show-modal"); // use All to select all elements with this clsss and not just the first one.
