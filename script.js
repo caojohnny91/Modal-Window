fee933ba6ab631784da21f7b7b4dd2bb7d0f76bf
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-23 08:35:35 -0500

    create eventListener to remove class of 'hidden' when modal btn is clicked

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal"); // use All to select all elements with this clsss and not just the first one.
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", function () {
+    // testing with clog to make sure that each show modal btn is affected with this for loop
+    console.log("Button Clicked");
+    modal.classList.remove("hidden"); // removing the class name of 'hidden' when the show-modal btn is clicked (by removing this class, the modal will be able to be seen)
+  });
+}
